0cf9b9c693d51be563b046bee7ffb1a87bef016f
Alisher Zinullayev <zinullayev09@gmail.com>
2024-10-04 00:59:00 +0500

    project init

---
+++ Real-Time-Stock-Tracker-App/StocksListViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Real-Time-Stock-Tracker-App
+//
+//  Created by Alisher Zinullayev on 04.10.2024.
+//
+
+import UIKit
+
+final class StocksListViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Stocks"
+        view.backgroundColor = .systemBackground
+    }
+}
+
