73b2ae93e860cbdd8907efa35c7d78fbe8ebadff
Alisher Zinullayev <zinullayev09@gmail.com>
2024-11-23 21:24:21 +0500

    small refactoring

--- Real-Time-Stock-Tracker-App/Services/StockPriceLoader.swift
+++ Real-Time-Stock-Tracker-App/Services/StockPriceLoader.swift
@@ -8,6 +8,12 @@
 import Foundation
 
 final class StockPriceLoader: StockPriceLoaderProtocol {
+    private let networkClient: NetworkClientProtocol
+    
+    init(networkClient: NetworkClientProtocol = NetworkingClient()) {
+        self.networkClient = networkClient
+    }
+    
     func fetchStockPrice(ticker: String, completion: @escaping (Result<StockPriceData, NetworkingError>) -> Void) {
         guard let url = URL(string: "\(APIConstants.baseURL)quote?symbol=\(ticker)&token=\(APIConstants.API_KEY)") else {
             completion(.failure(.invalidURL))
@@ -16,27 +22,9 @@ final class StockPriceLoader: StockPriceLoaderProtocol {
 
         var request = URLRequest(url: url)
         request.httpMethod = "GET"
-
-        let task = URLSession.shared.dataTask(with: request) { data, response, error in
-            if let error = error {
-                completion(.failure(.networkError(error)))
-                return
-            }
-
-            guard let data = data else {
-                completion(.failure(.invalidData))
-                return
-            }
-
-            do {
-                let decoder = JSONDecoder()
-                let stockResponse = try decoder.decode(StockPriceData.self, from: data)
-                completion(.success(stockResponse))
-            } catch {
-                completion(.failure(.decodingError(error)))
-            }
-        }
         
-        task.resume()
+        networkClient.performRequest(with: request) { (result: Result<StockPriceData, NetworkingError>) in
+            completion(result)
+        }
     }
 }
